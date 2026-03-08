29021bddf2448afa5aacda276dd2270f3742bf22
Sonnaly <sonnalyhenri7@gmail.com>
2026-03-08 19:31:32 -0300

    fix porta para deploy

--- backend/server.js
+++ backend/server.js
@@ -30,9 +30,11 @@ app.post("/humor", (req, res) => {
     res.json({status: "salvo"})
 })
 
-app.listen(3000, () => {
-    console.log("Servidor rodando em http://localhost:3000")
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor rodando na porta " + PORT);
+});
 
 app.delete("/humor/:id", (req, res) => {
 
